c7b8c83e5cd676c2320a8ccc1d65f2172f923630
paris <paris@helloworld.com>
2016-11-02 22:38:20 +0700

    only enable the button XemKetQua if Diem is a real number

--- XemKetQua/XemKetQua/ViewController.swift
+++ XemKetQua/XemKetQua/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var txtDiem: UITextField!
     @IBOutlet weak var lblKetQua: UILabel!
+    @IBOutlet weak var btnXemKetQua: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnXemKetQua.isEnabled = false
+        txtDiem.delegate = self
+        lblKetQua.backgroundColor = UIColor.clear
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,9 +32,23 @@ class ViewController: UIViewController {
         let diem = Float(txtDiem.text!)
         if (diem! >= 5){
             lblKetQua.text = "Ket qua: Dau roi!"
+            lblKetQua.backgroundColor = UIColor.cyan
         }else{
             lblKetQua.text = "Ket qua: Tieu roi Luom oi!"
+            lblKetQua.backgroundColor = UIColor.red
         }
+    }
+    
+    //Ref: http://stackoverflow.com/questions/26919854/how-can-i-declare-that-a-text-field-can-only-contain-an-integer
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let text = textField.text
+        print("text: " + text!)
+        if (Float(text!) != nil){
+            btnXemKetQua.isEnabled = true
+        }else{
+            btnXemKetQua.isEnabled = false;
+        }
+        return true
     }
 
 }
